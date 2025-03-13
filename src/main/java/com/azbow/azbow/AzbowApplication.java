81c9bab413e20602b3fece5f8fb466ca18534710
danukaem <danukaem@gmail.com>
2025-03-13 17:02:14 +0530

    first commit

---
+++ src/main/java/com/azbow/azbow/AzbowApplication.java
@@ -0,0 +1,13 @@
+package com.azbow.azbow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzbowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzbowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/azbow/azbow/AzbowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azbow.azbow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzbowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
